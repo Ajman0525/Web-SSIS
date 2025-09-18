26afc9008cb3ead2be4ae831aff971bf2998a70e
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-09-18 21:07:20 +0800

    Added functionality for the navigation links

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    return render_template("home.html")
+
+@app.route("/students")
+def students():
+    return render_template("students.html")
+
+@app.route("/programs")
+def programs():
+    return render_template("programs.html")
+
+@app.route("/colleges")
+def colleges():
+    return render_template("colleges.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

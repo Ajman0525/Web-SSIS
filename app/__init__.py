58ecd858979458048ccdf44d3a2ec33b544083a6
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-01 15:03:13 +0800

    feat: add typing animation on home page
    Integrated a Homepage header with a typing animation that also says the user on the current session.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, session, url_for
 from app.college import college_blueprint
 from app.program import program_blueprint
 from app.student import student_blueprint
+# from app.home import home_blueprint
 from app.user import user_blueprint
 from app.database import init_app
 from dotenv import load_dotenv
@@ -16,12 +17,14 @@ def create_app():
     app.register_blueprint(program_blueprint)
     app.register_blueprint(student_blueprint)
     app.register_blueprint(user_blueprint)
+    # app.register_blueprint(home_blueprint)
 
 
     @app.route("/")
     def home():
-        if "user_id" in session: 
-            return render_template("home.html")
+        if "user_id" in session:
+            user_name = session.get("username", "User")
+            return render_template("home.html", user_name=user_name)
         return redirect(url_for("user.login"))
 
     init_app(app)

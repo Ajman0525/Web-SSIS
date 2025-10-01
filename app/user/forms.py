21fbabfaaae2ff4e64da82d81e48c91ffc4cae65
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-01 21:18:33 +0800

    feat: validator on login and signup forms

--- app/user/forms.py
+++ app/user/forms.py
@@ -1,4 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, validators
+from wtforms import StringField, PasswordField, validators
 
-# input validators
+class SignupForm(FlaskForm):
+    username = StringField("Username", [
+        validators.DataRequired(),
+        validators.Length(min=3, max=25)
+    ])
+    email = StringField("Email", [
+        validators.DataRequired(),
+        validators.Length(min=6, max=50)
+    ])
+    password = PasswordField("Password", [
+        validators.DataRequired(),
+        validators.Length(min=6) 
+    ])
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", [
+        validators.DataRequired(),
+        validators.Length(min=3, max=25)
+    ])
+    password = PasswordField("Password", [
+        validators.DataRequired()
+    ])

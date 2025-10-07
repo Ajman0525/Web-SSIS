1e6b2d19cbb8d9a080ea16beaf9921b91f9bbf9e
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-08 00:07:35 +0800

    feat: link progress bar to database for real-time data updates

--- app/home/controller.py
+++ app/home/controller.py
@@ -10,12 +10,45 @@ def home():
 
     db = get_db()
     cursor = db.cursor()
+
+    # ----- STUDENTS ----- #
+    cursor.execute("SELECT COUNT(*) FROM students WHERE program IS NOT NULL")
+    enrolled_students = cursor.fetchone()[0] 
+
     cursor.execute("SELECT COUNT(*) FROM students")
     total_students = cursor.fetchone()[0]
+
+    student_percentage = (
+        round((enrolled_students / total_students) * 100, 1)
+        if total_students > 0 else 0
+    )
+    # ----- PROGRAMS ----- #
+    cursor.execute("SELECT COUNT(*) FROM programs WHERE college_code IS NOT NULL")
+    active_programs = cursor.fetchone()[0]
+
     cursor.execute("SELECT COUNT(*) FROM programs")
     total_programs = cursor.fetchone()[0]
+
+    program_percentage = (
+        round((active_programs / total_programs) * 100, 1)
+        if total_programs > 0 else 0
+    )
+
+    # ----- COLLEGES ----- #
     cursor.execute("SELECT COUNT(*) FROM colleges")
     total_colleges = cursor.fetchone()[0]
+    
+    cursor.execute("""
+                        SELECT COUNT(DISTINCT c.code)
+                        FROM colleges c
+                        JOIN programs p ON p.college_code = c.code
+                    """)
+    active_colleges = cursor.fetchone()[0]
+
+    college_percentage = (
+        round((active_colleges / total_colleges) * 100, 1)
+        if total_colleges > 0 else 0
+    )
 
     user_name = session.get("username")
     return render_template(
@@ -23,5 +56,8 @@ def home():
         user_name=user_name,
         total_students=total_students,
         total_programs=total_programs,
-        total_colleges=total_colleges
+        total_colleges=total_colleges,
+        student_percentage=student_percentage,
+        program_percentage=program_percentage,
+        college_percentage=college_percentage
     )
